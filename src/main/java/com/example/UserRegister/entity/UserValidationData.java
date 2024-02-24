efd3148e5a8b2617b086d11081d3b23b5fad8ac5
arkadii_strelbitskii <foxyincube@icloud.com>
2024-02-24 14:50:50 +0600

    При вводе неправилного телефона и почти регистрация не проиходит

--- src/main/java/com/example/UserRegister/entity/UserValidationData.java
+++ src/main/java/com/example/UserRegister/entity/UserValidationData.java
@@ -15,12 +15,12 @@ public class UserValidationData {
 
     private Long id;
 
-    @NotEmpty(message = "Email is empty!")
-    @Email(regexp = "^[a-zA-Z0-9].[a-zA-Z0-9\\._%\\+\\-]{0,63}@[a-zA-Z0-9\\.\\-]+\\.[a-zA-Z]{2,30}$")
+    @NotEmpty
+    @Email(regexp = "^[a-zA-Z0-9].[a-zA-Z0-9\\._%\\+\\-]{0,63}@[a-zA-Z0-9\\.\\-]+\\.[a-zA-Z]{2,30}$", message = "E-mail is not valid")
     private String email;
 
-    @NotEmpty(message = "Phone is empty!")
-    @Pattern(regexp = "^(?<code>\\+?\\d{1,3})[-\\s]{0,}(?<number>\\(?\\d{3}\\)?[-\\s]{0,}\\d{3}[-\\s]{0,}\\d{2}[-\\s]{0,}\\d{2})$")
+    @NotEmpty
+    @Pattern(regexp = "^(?<code>\\+?\\d{1,3})[-\\s]{0,}(?<number>\\(?\\d{3}\\)?[-\\s]{0,}\\d{3}[-\\s]{0,}\\d{2}[-\\s]{0,}\\d{2})$", message = "Phone is not valid.")
     private String phone;
 
     @NotEmpty(message = "Username is empty!")
